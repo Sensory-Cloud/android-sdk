d4f1a465624e817e7652d8131e67fae8cf167636
Niles Hacking <nhacking@sensoryinc.com>
2021-11-30 15:09:01 -0700

    OAuth Service

---
+++ SensoryCloud/src/main/java/inc/sensory/sensorycloud/service/OAuthService.java
@@ -0,0 +1,103 @@
+package inc.sensory.sensorycloud.service;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import io.sensory.api.common.GenericClient;
+import io.sensory.api.common.TokenResponse;
+import io.sensory.api.oauth.OauthServiceGrpc;
+import io.sensory.api.oauth.TokenRequest;
+import io.sensory.api.v1.management.DeviceResponse;
+import io.sensory.api.v1.management.DeviceServiceGrpc;
+import io.sensory.api.v1.management.EnrollDeviceRequest;
+
+public class OAuthService {
+
+    public interface EnrollDeviceListener {
+        void onSuccess(DeviceResponse response);
+        void onFailure(Throwable t);
+    }
+
+    public interface GetTokenListener {
+        void onSuccess(TokenResponse response);
+        void onFailure(Throwable t);
+    }
+
+    public void enrollDevice(
+            String url, // TODO - config?
+            String deviceID, // TODO - config?
+            String tenantID, // TODO - config?
+            String name,
+            String credential,
+            String clientID,
+            String clientSecret,
+            EnrollDeviceListener listener ) {
+
+        // ManagedChannel managedChannel = ManagedChannelBuilder.forTarget(url).useTransportSecurity().build();
+        ManagedChannel managedChannel = ManagedChannelBuilder.forTarget(url).usePlaintext().build();
+
+        DeviceServiceGrpc.DeviceServiceStub deviceServiceStub = DeviceServiceGrpc.newStub(managedChannel);
+        GenericClient genericClient = GenericClient.newBuilder()
+                .setClientId(clientID)
+                .setSecret(clientSecret)
+                .build();
+        EnrollDeviceRequest enrollDeviceRequest = EnrollDeviceRequest.newBuilder()
+                .setName(name)
+                .setDeviceId(deviceID)
+                .setTenantId(tenantID)
+                .setClient(genericClient)
+                .setCredential(credential)
+                .build();
+
+        StreamObserver<DeviceResponse> responseObserver = new StreamObserver<DeviceResponse>() {
+            @Override
+            public void onNext(DeviceResponse value) {
+                listener.onSuccess(value);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                listener.onFailure(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                managedChannel.shutdown();
+            }
+        };
+
+        deviceServiceStub.enrollDevice(enrollDeviceRequest, responseObserver);
+    }
+
+    // TODO: - url config?
+    public void getToken(String url, String clientID, String secret, GetTokenListener listener) {
+        // ManagedChannel managedChannel = ManagedChannelBuilder.forTarget(url).useTransportSecurity().build();
+        ManagedChannel managedChannel = ManagedChannelBuilder.forTarget(url).usePlaintext().build();
+
+        OauthServiceGrpc.OauthServiceStub client = OauthServiceGrpc.newStub(managedChannel);
+
+        TokenRequest tokenRequest = TokenRequest.newBuilder()
+                .setClientId(clientID)
+                .setSecret(secret)
+                .build();
+
+        StreamObserver<TokenResponse> responseObserver = new StreamObserver<TokenResponse>() {
+            @Override
+            public void onNext(TokenResponse value) {
+                listener.onSuccess(value);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                listener.onFailure(t);
+            }
+
+            @Override
+            public void onCompleted() {
+                managedChannel.shutdown();
+            }
+        };
+
+        client.getToken(tokenRequest, responseObserver);
+    }
+}
--- app/src/main/java/inc/sensory/sensoryclouddemo/FirstFragment.java
+++ app/src/main/java/inc/sensory/sensoryclouddemo/FirstFragment.java
@@ -1,5 +1,8 @@
 package inc.sensory.sensoryclouddemo;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +12,10 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import inc.sensory.sensorycloud.service.OAuthService;
 import inc.sensory.sensoryclouddemo.databinding.FragmentFirstBinding;
+import io.sensory.api.common.TokenResponse;
+import io.sensory.api.v1.management.DeviceResponse;
 
 public class FirstFragment extends Fragment {
 
@@ -32,8 +38,56 @@ public class FirstFragment extends Fragment {
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(FirstFragment.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                attemptEnrollDevice();
+//                NavHostFragment.findNavController(FirstFragment.this)
+//                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+    public void attemptEnrollDevice() {
+        OAuthService service = new OAuthService();
+
+        service.enrollDevice(
+                "10.0.2.2:50051",
+                "abcdef01-2345-6789-0123-456789abcdef",
+                "b6e1b848-75da-46cb-aad8-981cc3ccebcd",
+                "Niles Android",
+                "Enroll123Sensory!!",
+                "95bd507d-e200-4721-87d5-bfdcfa3f5ab4",
+                "407833043d144c7c8c866c97575ef3f1",
+                new OAuthService.EnrollDeviceListener() {
+            @Override
+            public void onSuccess(DeviceResponse response) {
+                System.out.println("Received enrollment response");
+                attemptGetToken();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println(t.getMessage());
+                t.printStackTrace();
+            }
+        });
+
+    }
+
+    public void attemptGetToken() {
+        OAuthService service = new OAuthService();
+
+        service.getToken(
+                "10.0.2.2:50051",
+                "95bd507d-e200-4721-87d5-bfdcfa3f5ab4",
+                "407833043d144c7c8c866c97575ef3f1",
+                new OAuthService.GetTokenListener() {
+            @Override
+            public void onSuccess(TokenResponse response) {
+                System.out.println(response.getAccessToken());
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println(t.getMessage());
             }
         });
     }
