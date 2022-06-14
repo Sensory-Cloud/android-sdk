d98e0e414c03d71d7cab4d0e212fecf20a4fceed
Niles Hacking <nhacking@sensoryinc.com>
2022-06-14 15:36:31 -0600

    Modifying secure credential storage to be able to save arbitrary data

--- SensoryCloud/src/androidTest/java/ai/sensorycloud/tokenManager/DefaultSecureCredentialStoreTest.java
+++ SensoryCloud/src/androidTest/java/ai/sensorycloud/tokenManager/DefaultSecureCredentialStoreTest.java
@@ -9,11 +9,15 @@ import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 
 import static org.mockito.Mockito.*;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
@@ -34,9 +38,19 @@ public class DefaultSecureCredentialStoreTest extends TestCase {
     }
 
     @Test
-    public void testGetCredentials() throws Exception {
+    public void testSetGetCredentials() throws Exception {
         ArgumentCaptor<String> keyCaptor = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        try {
+            credentialStore.getClientId();
+            fail("Call should throw when no saved credentials");
+        } catch (Exception ignored) {}
+
+        try {
+            credentialStore.getClientSecret();
+            fail("Call should throw when no saved credentials");
+        } catch (Exception ignored) {}
 
         credentialStore.setCredentials("client", "secret");
 
@@ -52,5 +66,59 @@ public class DefaultSecureCredentialStoreTest extends TestCase {
         String finalSecret = credentialStore.getClientSecret();
         assertEquals("clientID should not be mutated", "client", finalClient);
         assertEquals("client secret should not be mutated", "secret", finalSecret);
+    }
+
+    @Test
+    public void testSaveGetData() throws Exception {
+        ArgumentCaptor<String> keyCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        byte[] initialData = "MockData".getBytes(StandardCharsets.UTF_8);
+        credentialStore.saveData("unitTest", initialData);
+
+        verify(mockEditor, times(2)).putString(keyCaptor.capture(), valueCaptor.capture());
+        List<String> keys = keyCaptor.getAllValues();
+        List<String> values = valueCaptor.getAllValues();
+
+        for (int i = 0; i < keys.size(); i++) {
+            when(mockPrefs.getString(keys.get(i), "")).thenReturn(values.get(i));
+        }
+
+        byte[] finalData = credentialStore.loadData("unitTest");
+        assertTrue("Data should not be mutated", Arrays.equals(initialData, finalData));
+
+        try {
+            credentialStore.loadData("bogus");
+            fail("Call should throw when no saved data");
+        } catch (Exception ignored) {}
+    }
+
+    @Test
+    public void testDeleteData() throws Exception {
+        ArgumentCaptor<String> keyCaptor = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> valueCaptor = ArgumentCaptor.forClass(String.class);
+
+        byte[] initialData = "MockData".getBytes(StandardCharsets.UTF_8);
+        credentialStore.saveData("deleteUnitTest", initialData);
+
+        verify(mockEditor, times(2)).putString(keyCaptor.capture(), valueCaptor.capture());
+        List<String> keys = keyCaptor.getAllValues();
+        List<String> values = valueCaptor.getAllValues();
+
+        for (int i = 0; i < keys.size(); i++) {
+            when(mockPrefs.getString(keys.get(i), "")).thenReturn(values.get(i));
+        }
+
+        credentialStore.deleteData("deleteUnitTest");
+        verify(mockEditor, times(2)).remove(Mockito.anyString());
+
+        try {
+            byte[] data = credentialStore.loadData("deleteUnitTest");
+            System.out.println(new String(data, StandardCharsets.UTF_8));
+            fail("Data should be deleted");
+        } catch (Exception ignored) {}
+
+         // No error should be thrown on deleting an already deleted item
+         credentialStore.deleteData("deleteUnitTest");
     }
 }
--- SensoryCloud/src/main/java/ai/sensorycloud/tokenManager/SecureCredentialStore.java
+++ SensoryCloud/src/main/java/ai/sensorycloud/tokenManager/SecureCredentialStore.java
@@ -17,4 +17,27 @@ public interface SecureCredentialStore {
      * @throws Exception if an error occurs while loading from the secure credential store or if the item is not found
      */
     String getClientSecret() throws Exception;
+
+    /**
+     * Securely saves arbitrary data in secure storage. This is used for saving enrollment tokens on device
+     * @param id string identifier to save the data under
+     * @param data data to securely save
+     * @throws Exception if an error occurs while saving to secure storage
+     */
+    void saveData(String id, byte[] data) throws Exception;
+
+    /**
+     * Loads arbitrary data from secure storage. This is used for loading enrollment tokens on device
+     * @param id string identifier of the data to load
+     * @return the saved data
+     * @throws Exception if an error occurs while loading from the secure credential store or if the item is not found
+     */
+    byte[] loadData(String id) throws Exception;
+
+    /**
+     * Deletes saved data from secure storage
+     * @param id string identifier of the data to delete
+     * @throws Exception if an error occurs while deleting the data. No error is thrown if the item is not found
+     */
+    void deleteData(String id) throws Exception;
 }
